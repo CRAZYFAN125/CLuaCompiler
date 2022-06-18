ef713ee269da0847ceaf07707841540990a38356
CRAZYFAN125 <88847481+CRAZYFAN125@users.noreply.github.com>
2022-06-18 20:19:53 +0200

    Init

---
+++ CLuaCompiler/CLuaCompiler/Compiler.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace Crazy.CLuaCompiler
+{
+    public class Compiler
+    {
+        public void Compile(string DataFromFile)
+        {
+            Dictionary<int,Dictionary<string,string>> Instructions = new Dictionary<int,Dictionary<string,string>>();
+            Functions.GetValuesFromString(DataFromFile, out Instructions);
+
+
+        }
+    }
+}
